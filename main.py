021c5652e514b41041135e44dec430402f993a36
João Vitor <jvsantillo@gmail.com>
2024-02-17 12:31:48 -0300

    Dia 17/02

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from dotenv import load_dotenv
 import os
 import csv
 import streamlit as st
-from langchain_community.document_loaders import CSVLoader
+from langchain_community.document_loaders import CSVLoader, UnstructuredExcelLoader
 from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import Chroma
-from langchain.text_splitter import CharacterTextSplitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.docstore.document import Document 
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser, CommaSeparatedListOutputParser
@@ -15,75 +15,100 @@ from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains import create_retrieval_chain
 from langchain_openai import ChatOpenAI
 
+class PreProcessing:
+    
+    @staticmethod
+    def load_openAIKey():
+        if os.getenv("OPENAI_API_KEY") is None or os.getenv("OPENAI_API_KEY") == "":
+            print("OPENAI_API_KEY is not set")
+            exit(1)
+        else:
+            print("OPENAI_API_KEY is set")
+    
+    @staticmethod
+    def load_langSmithKey():
+        if os.getenv("LANGCHAIN_API_KEY") is None or os.getenv("LANGCHAIN_API_KEY") == "":
+            print("LANGCHAIN_API_KEY is not set")
+            exit(1)
+        else:
+            print("LANGCHAIN_API_KEY is set")
+
+    def set_streamlit(self,page_title,header):
+        st.set_page_config(page_title)
+        st.header(header)
+
+    def start_llm(self):
+        llm = ChatOpenAI(model="gpt-4-turbo-preview")
+        return llm
+
+    def load_context(_self, _url):
+        loader = UnstructuredExcelLoader(_url)
+        docs = loader.load()
+        return docs
+
+    @st.cache_resource
+    def split_context(_self, _docs, _chunk_size):
+
+        if _chunk_size is not None:
+            splitter = RecursiveCharacterTextSplitter(separators= ["\n\n\n", "\n\n\n\n"],
+                                        chunk_size=_chunk_size, 
+                                        chunk_overlap=0)
+        
+            documents = splitter.split_documents(_docs)
+
+        st.write("Foram importados " + str(len(documents)) + " eventos para o contexto da aplicação")
+
+        return documents
+
+    @st.cache_resource
+    def embed_context(_self, _documents):
+        # Embedding prior to VectorStore:
+        embeddings = OpenAIEmbeddings()
+        vectorstore = Chroma.from_documents(_documents, embeddings, persist_directory="./chroma_db")
+        return vectorstore
+
+
 def main():
     load_dotenv()
+    pp = PreProcessing()
 
-    # Load the OpenAI API key from the environment variable:
-    if os.getenv("OPENAI_API_KEY") is None or os.getenv("OPENAI_API_KEY") == "":
-        print("OPENAI_API_KEY is not set")
-        exit(1)
-    else:
-        print("OPENAI_API_KEY is set")
+    pp.load_langSmithKey()
+    pp.load_openAIKey()
+    pp.set_streamlit("COLAB IA 💰","COLAB AI - De x Para de Eventos 💰")
+    llm = pp.start_llm()
 
-    # Streamlit page configs:
-    st.set_page_config(page_title="COLAB IA 💰")
-    st.header("COLAB AI - De x Para de Eventos 💰")
-
-    # Initializing llm:
-    llm = ChatOpenAI(model='gpt-4-turbo-preview')
+    chunk_size = st.number_input("Informe o tamanho do chunk: ", min_value=120, max_value=200, 
+                                    help = '''O chunk é um parâmetro de importação que carrega 
+                                    cada linha do DexPara como um documento para o BD Vetorial. 
+                                    Informar a quantidade média de caracteres de uma linha do DexPara 
+                                    é suficiente para esse parâmetro''')
     
-    
-
-    # Define the columns we want to embed vs which ones we want in metadata:
-    columns_to_embed = ["CODIGO_FOLHA_DE","DESCRICAO_EVENTO_DE","TIPO_EVENTO_DE", "DESCRICAO_EVENTO_PARA", "TIPO_EVENTO_PARA", "DETERMINACAO_PARA", "DESCRICAO_EVENTO_REFERENCIA"]
-    columns_to_metadata = ["CODIGO_EVENTO_DE","DESCRICAO_EVENTO_DE", "TIPO_EVENTO_DE", "CODIGO_FOLHA_DE", "INSS_DE", "FGTS_DE","IRRF_DE", "MEDIAVEL_DE", "CODIGO_EVENTO_PARA","DESCRICAO_EVENTO_PARA","TIPO_EVENTO_PARA", "CODIGO_FOLHA_PARA","DETERMINACAO_PARA", "CODIGO_EVENTO_REFERENCIA","DESCRICAO_EVENTO_REFERENCIA", "INSS_PARA", "FGTS_PARA","IRRF_PARA", "MEDIAVEL_PARA"]
-    
-    # Process the CSV into the embedable content vs the metadata and put it into Document format so that we can chunk it into pieces.
-    """docs = []
-    with open('Planilha_DexPara_Bravo.csv', encoding='utf-8') as csvfile:
-        csv_reader = csv.DictReader(csvfile)
-        for i, row in enumerate(csv_reader):
-            to_metadata = {col: row[col] for col in columns_to_metadata if col in row}
-            values_to_embed = {k: row[k] for k in columns_to_embed if k in row}
-            to_embed = "\n".join(f"{k.strip()}: {v.strip()}" for k, v in values_to_embed.items())
-            newDoc = Document(page_content=to_embed, metadata=to_metadata)
-            docs.append(newDoc)"""
-    
-    # Loading context from csv file:
-    loader = CSVLoader(file_path='C:\\Users\\joao.santillo\\OneDrive - LG INFORMATICA S.A\\COLAB IA\\Langchain\\langchain-ask-csv\\Planilha_DexPara_Bravo.csv')
-    docs = loader.load()
-
-    st.write(docs)
-
-    # Splitting the document using Chracter splitting:
-    
-    splitter = CharacterTextSplitter(separator="\n",
-                                chunk_size=500, 
-                                chunk_overlap=0,
-                                length_function=len)
-    
-    documents = splitter.split_documents(docs)
-
-    # Embedding prior to VectorStore:
-    embeddings = OpenAIEmbeddings()
-    vectorstore = Chroma.from_documents(documents, embeddings)
+    docs = pp.load_context("files\Planilha_DexPara_Bravo.xlsx")
+    documents = pp.split_context(docs,chunk_size)
+    vectorstore = pp.embed_context(documents)
 
     # Creating document chain:
-    query = ""
-
     template = '''
-    Você é um consultor de folha de pagamento, está em um projeto de migração de dados e ficou responsável pela migração da 
-    ficha financeira histórica do fornecedor atual para o sistema da empresa de software que você trabalha. 
-    A sua tarefa é para um certo evento DE de folha de pagamento fornecido, 
-    definir um evento de folha de pagamento PARA com base no contexto fornecido. 
-    Se não souber a resposta, informe que não encontrou um evento correspondente
-    Responda a seguinte pergunta baseado somente no contexto fornecido: 
-    
-    <context>
-    {context}
-    </context>
+    Objetivo da Conversa:
+        Identificar o evento de folha de pagamento PARA apropriado com base no código do evento fornecido pelo usuário.
 
-    Question: {input}
+    Contexto Inicial:
+        Você forneceu uma planilha Excel com as seguintes colunas:
+        ["CODIGO_EVENTO_DE","DESCRICAO_EVENTO_DE", "TIPO_EVENTO_DE", "CODIGO_FOLHA_DE", "INSS_DE", "FGTS_DE","IRRF_DE", "MEDIAVEL_DE", "CODIGO_EVENTO_PARA","DESCRICAO_EVENTO_PARA","TIPO_EVENTO_PARA", "CODIGO_FOLHA_PARA","DETERMINACAO_PARA", "CODIGO_EVENTO_REFERENCIA","DESCRICAO_EVENTO_REFERENCIA", "INSS_PARA", "FGTS_PARA","IRRF_PARA", "MEDIAVEL_PARA"]
+        {context}
+
+    Pergunta a ser respondida:
+        Baseado no código do evento fornecido pelo usuário, identifique o evento de folha de pagamento PARA apropriado para a migração de dados.
+        {input}
+
+    Instruções para o ChatGPT:
+        Use a planilha fornecida como contexto para identificar o evento de folha de pagamento PARA apropriado com base no código do evento fornecido pelo usuário.
+        Responda como uma tabela.
+
+    Informações Importantes:
+        O evento de folha de pagamento PARA deve ser identificado com base nas informações fornecidas nas colunas da planilha.
+        Forneça uma resposta que seja relevante e apropriada para o código do evento fornecido pelo usuário. 
+        Forneça apenas a tabela, não gere texto adicional.
     '''
 
     prompt = ChatPromptTemplate.from_template(template)
@@ -97,7 +122,8 @@ def main():
     #output_parser = CommaSeparatedListOutputParser()
 
     #representa o arquivo "DE" sem estar preenchido com o Para
-    csv_file = st.file_uploader("Importe um De", type="csv")
+    #csv_file = st.file_uploader("Importe um De", type="csv")
+    csv_file = True
 
     if csv_file is not None:
         
